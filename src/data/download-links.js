05c02c2a7af69bb18148f2b94c0ec9cb86986447
Jaime de Venecia <deveneciaj@jdv-plus-2022.lan>
2023-09-07 16:27:08 -0700

    Update Mac build to Intel version to support all Macs instead of just M1 chips

--- src/data/download-links.js
+++ src/data/download-links.js
@@ -1,4 +1,4 @@
 export const downloadLinks = {
-  macOS: 'https://github.com/open-source-labs/OverVue/releases/download/v10.0.0/OverVue-10.0.0-arm64-mac.zip',
+  macOS: 'https://github.com/open-source-labs/OverVue/releases/download/v10.0.0/OverVue-10.0.0-intel-mac.zip',
   linux: 'https://github.com/open-source-labs/OverVue/releases/download/v10.0.0/overvue_10.0.0_amd64.deb'
 };
